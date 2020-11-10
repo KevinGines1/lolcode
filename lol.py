74b6c70009d5be656548166ee959409e314d4bf0
KevinGines1 <kagines1@up.edu.ph>
2020-11-11 01:24:04 +0800

    begin implementation for GUI; .lol files can be selected

--- lol.py
+++ lol.py
@@ -1,1 +1,33 @@
-print("LOLterpreter!")
+# print("LOLterpreter!")
+from tkinter import * 
+from tkinter import filedialog
+from tkinter import ttk
+
+#* -- GUI INITIALIZATIONS --
+window = Tk()
+window.geometry("1450x750") #widthxheight
+window.title("Ang ganda ni Maam Kat LOLTERPRETER")
+
+#* -- CLASSES --
+class SourceCode():
+    def __init__(self):
+        # constructor
+
+#* -------------
+#* -- FUNCTIONS --
+def selectSourceCode():
+    # pass
+    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files", "*.lol*"),("all files","*.*")))
+    # print(filename)
+    # TODO: create a class (SourceCode) for the code input
+    # TODO: store the source code by using SourceCode.addCode()
+#* ---------------
+
+
+#* -- GUI ELEMENTS --
+selectSourceCode = Button(window, text="Select LOLCode file", width = 19, command=selectSourceCode)
+selectSourceCode.place(x=10, y=20)
+
+
+
+window.mainloop()
