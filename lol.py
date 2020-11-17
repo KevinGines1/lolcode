bcac9fb67ec56434c129df06b0a3cd9c5ed5705b
KevinGines1 <kagines1@up.edu.ph>
2020-11-17 23:47:41 +0800

    create all the necessary GUI elements and group them through classes; display loaded .lol file in GUI

--- lol.py
+++ lol.py
@@ -5,28 +5,109 @@ from tkinter import ttk
 
 #* -- GUI INITIALIZATIONS --
 window = Tk()
-window.geometry("1450x750") #widthxheight
+window.geometry("1200x750") #widthxheight
 window.title("Ang ganda ni Maam Kat LOLTERPRETER")
+    
+    # TODO: add methods to the GUIs
 
 #* -- CLASSES --
-class SourceCode():
+class SourceCode(): # * class for the source code
+    # constructor
     def __init__(self):
-        # constructor
+        self.code = "" # initialize the holder for the entire code
+        self.lexemes = [] # initialize the list of lexemes
 
+    def addCode(self, code): # set the code for object
+        self.code = code
+    
+    def getCode(self):
+        return self.code
+
+    def showMe(self):
+        print("=====SOURCE CODE=====")
+        print("THE CODE: ")
+        print(self.code)
+        print("---------------------")
+        print("LEXEMES")
+        print(self.lexemes)
+        print("=====================")
+
+class SelectGUI(): # * class for grouping the select button and displaying the code uploaded
+    def __init__(self):
+        # select button
+        self.selectSourceCode = Button(window, text="Select LOLCode file", width = 19, command=selectSourceCode)
+        self.selectSourceCode.place(x=10, y=20, width=325)
+        # textbox where the code will be displayed
+        self.codeDisplay = Text(window, width=40, height=20, background="white")
+        self.codeDisplay.place(x=10, y=50)
+
+    def setCodeDisplay(self, code):
+        self.codeDisplay.insert(END, code)
+
+class TablesGUI(): # * class for accessing and displaying the lexemes and symbol table
+    def __init__(self):
+        Label(window, text="LOL CODE Interpreter", font="none 19 bold", bg="black", fg="white").place(x=350, y=20, width=830)
+        # lexemes table
+        Label(window, text="Lexemes", font="none 15").place(x=510, y=60)
+        self.lexTable = ttk.Treeview(window, columns=("Lexemes", "Classification"), show="headings")
+        self.lexTableScroll = ttk.Scrollbar(window, orient="vertical", command=self.lexTable.yview)
+        self.lexTable.column("Lexemes", width=200)
+        self.lexTable.column("Classification", width=200)
+        self.lexTable.heading("Lexemes", text="Lexemes")
+        self.lexTable.heading("Classification", text="Classification")
+        self.lexTable.place(x=350, y=90, height="282")
+        self.lexTableScroll.place(x=753, y=90, height=282)
+        self.lexTable.configure(yscrollcommand=self.lexTableScroll.set)
+
+        # symbol table
+        Label(window, text="Symbol Table", font="none 15").place(x=910, y=60)
+        self.symbolTable = ttk.Treeview(window, columns=("Identifier", "Value"), show="headings")
+        self.symbolTableScroll = ttk.Scrollbar(window, orient="vertical", command=self.symbolTable.yview)
+        self.symbolTable.column("Identifier", width=200)
+        self.symbolTable.column("Value", width=200)
+        self.symbolTable.heading("Identifier", text="Identifier")
+        self.symbolTable.heading("Value", text="Value")
+        self.symbolTable.place(x=775, y=90, height="282")
+        self.symbolTableScroll.place(x=1180, y=90, height=282)
+        self.symbolTable.configure(yscrollcommand=self.symbolTableScroll.set)
+
+
+class TerminalGUI(): # * class for accessing and displaying the "terminal", where the output of the program will be displayed
+    def __init__(self):
+        IT = "" # this is the implicit IT variable
+
+        #execute button
+        self.executeButton = Button(window, text="EXECUTE", width = 19, command=executeCode)
+        self.executeButton.place(x=10, y= 375, width=1185)
+        #textbox
+        self.codeOutput = Text(window, background="white")
+        self.codeOutput.place(x=10, y=405, width=1185, height=325)
+
+        
 #* -------------
 #* -- FUNCTIONS --
+def executeCode(): #* function that executes the loaded code
+    print("EXECUTE CODE")
+    # ! thinking of whether we get the code in the textbox OR from the "Source Code" file
+
+def readCode(filename): # * function that reads the code in the passed filename
+    f = open(filename)
+    code = f.read() # read the code inside the text file
+    theCode.addCode(code) # add the code to the initialized object
+    theCode.showMe()
+    interpreter.setCodeDisplay(theCode.getCode())
 def selectSourceCode():
-    # pass
+    # make the dialog be strict in selecting files by only allowing .lol code to be selected
     filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files", "*.lol*"),("all files","*.*")))
-    # print(filename)
-    # TODO: create a class (SourceCode) for the code input
-    # TODO: store the source code by using SourceCode.addCode()
+    readCode(filename)
 #* ---------------
 
+# * initialize the objects for the program
+codeSelectAndDisplay = SelectGUI() # create a GUI
+theCode = SourceCode() # create an object for the input Source LOLCode
+lexAndSymbolTables = TablesGUI() # create an object for the tables to be generated
+terminal = TerminalGUI() # create an object for the "terminal"
 
-#* -- GUI ELEMENTS --
-selectSourceCode = Button(window, text="Select LOLCode file", width = 19, command=selectSourceCode)
-selectSourceCode.place(x=10, y=20)
 
 
 
